e5052016da0796ae59c08ca30d1a3d64c3168065
Colman Davenport <colmandavenport@gmail.com>
2022-09-04 17:47:16 -0400

    Customs in search bar patch

---
+++ Melon/Patch/FixSearchBarPatch.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Database;
+using Assets.Scripts.PeroTools.Commons;
+using Assets.Scripts.PeroTools.Managers;
+using Assets.Scripts.Structs.Modules;
+using Assets.Scripts.UI.Panels.PnlMusicTag;
+using HarmonyLib;
+
+namespace CustomAlbums.Patch
+{
+    /// <summary>
+    /// Tells the game to load all of CAM's language variants when the search bar asks for them.
+    /// Fixes the search bar being unable to properly detect custom charts.
+    /// </summary>
+    [HarmonyPatch(typeof(SearchResults), nameof(SearchResults.InitLanguageResources))]
+    internal static class FixSearchBarPatch
+    {
+        private static void Postfix() {
+            var config = Singleton<ConfigManager>.instance;
+            var album = config.GetConfigObject<DBConfigALBUM>(AlbumManager.Uid + 1);
+            album.GetAllLocal(new Il2CppSystem.Collections.Generic.List<DBConfigLocalALBUM>());
+        }
+    }
+}
