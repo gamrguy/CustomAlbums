5639e8a8403ddd3a5c099aa50686692b57997c95
Colman Davenport <colmandavenport@gmail.com>
2022-09-04 17:48:25 -0400

    Level designer priority patch

---
+++ Melon/Patch/FixLevelDesignerSearchPatch.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Database;
+using Assets.Scripts.PeroTools.Commons;
+using Assets.Scripts.PeroTools.Managers;
+using Assets.Scripts.Structs.Modules;
+using Assets.Scripts.UI.Panels.PnlMusicTag;
+using HarmonyLib;
+
+namespace CustomAlbums.Patch
+{
+    /// <summary>
+    /// Flips levelDesigner display priority.
+    /// Original behavior: levelDesigner overrides levelDesignerX
+    /// New behavior:      levelDesignerX overrides levelDesigner
+    /// 
+    /// This allows levelDesigner to be used as a "hidden" designer, for search purposes.
+    /// </summary>
+    [HarmonyPatch(typeof(SearchResults), nameof(SearchResults.PeroLevelDesigner))]
+    internal static class FixLevelDesignerSearchPatch
+    {
+        private static bool Prefix(ref bool __result, PeroPeroGames.PeroString peroString, MusicInfo musicInfo, string containsText) {
+            __result |= peroString.LowerContains(musicInfo.levelDesigner, containsText);
+            if(__result) return false;
+
+            for(int i = 0; i <= 4; i++) {
+                __result |= peroString.LowerContains(musicInfo.GetLevelDesignerStringByIndex(i), containsText);
+                if(__result) return false;
+            }
+            return false;
+        }
+
+        private static bool LowerContains(this PeroPeroGames.PeroString peroString, string compareText, string containsText) {
+            peroString.Clear();
+            peroString.Append(compareText);
+            peroString.ToLower();
+            return peroString.Contains(containsText);
+        }
+    }
+}
---
+++ Melon/Patch/FixLevelDesignerTextPatch.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Scripts.Database;
+using Assets.Scripts.PeroTools.Commons;
+using Assets.Scripts.PeroTools.Managers;
+using Assets.Scripts.UI.Panels.PnlMusicTag;
+using HarmonyLib;
+
+namespace CustomAlbums.Patch
+{
+    /// <summary>
+    /// Flips levelDesigner display priority.
+    /// Original behavior: levelDesigner overrides levelDesignerX
+    /// New behavior:      levelDesignerX overrides levelDesigner
+    /// 
+    /// This allows levelDesigner to be used as a "hidden" designer, for search purposes.
+    /// </summary>
+    [HarmonyPatch(typeof(MusicInfo), nameof(MusicInfo.GetLevelDesignerStringByIndex))]
+    internal static class FixLevelDesignerTextPatch
+    {
+        private static bool Prefix(MusicInfo __instance, ref string __result, int index) {
+            switch(index) {
+                case 1:
+                    __result = __instance.levelDesigner1;
+                    break;
+                case 2:
+                    __result = __instance.levelDesigner2;
+                    break;
+                case 3:
+                    __result = __instance.levelDesigner3;
+                    break;
+                case 4:
+                    __result = __instance.levelDesigner4;
+                    break;
+                case 5:
+                    __result = __instance.levelDesigner5;
+                    break;
+            }
+
+            if(string.IsNullOrEmpty(__result)) __result = __instance.levelDesigner;
+            if(string.IsNullOrEmpty(__result)) __result = "?????";
+            return false;
+        }
+    }
+}
